7dd57a39d22f868327d4666066984bc2029efa8f
toshi1023 <toshimura1992@gmail.com>
2021-07-19 13:51:16 +0000

    Login page implements

--- src/views/pages/home/Login.tsx
+++ src/views/pages/home/Login.tsx
@@ -3,7 +3,8 @@ import '../../../styles/common/common.scss';
 import '../../../styles/home/home.scss';
 import { Grid, Typography, Card, CardHeader, CardContent } from '@material-ui/core';
 import DisplayStyles from '../../../styles/common/displayMode';
-import loginpage from '../../../image/loginpage.jpeg';
+import loginpage_back from '../../../image/loginpage_back.jpg';
+import loginpage_front from '../../../image/loginpage_front.jpg';
 
 const Login: React.FC = () => {
     const displayStyles = DisplayStyles();
@@ -14,8 +15,15 @@ const Login: React.FC = () => {
             {/* PC版 & iPad版 */}
             <div className={displayStyles.sectionDesktop}>
                 <Grid container justify="center" className="formcontainer">
-                    <Grid item sm={5} md={6}>
-                        <img src={loginpage} className="loginpage" />
+                    <Grid item sm={5} md={6} className="login_image_area">
+                        <img src={loginpage_back} className="loginpage_back" />
+                        <img src={loginpage_front} className="loginpage_front" />
+                        <Typography className="wf-merienda">
+                            MemoryShareApp
+                        </Typography>
+                        <Typography className="wf-roundedmplus1c">
+                            仲間と一緒に思い出を共有しよう
+                        </Typography>
                     </Grid>
                     <Grid item sm={5} md={4}>
                         <Card className="card">
