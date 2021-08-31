390f82f3a3f1095891602698b38c27238b0e2f2f
toshi1023 <toshimura1992@gmail.com>
2021-08-31 11:01:37 +0000

    Talk implements

--- src/views/pages/home/Talk.tsx
+++ src/views/pages/home/Talk.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Grid, Typography, Hidden, Button, Box, Avatar, CardContent, IconButton, TextField } from '@material-ui/core';
 import ReplyIcon from '@material-ui/icons/Reply';
 import _ from 'lodash';
@@ -6,35 +6,53 @@ import _ from 'lodash';
 import talk_list from '../../../data/talk_list_data.json';
 
 const Talk: React.FC = () => {
+    const messageArea = useRef<HTMLDivElement>(null);
     const loginId = 1;
+
+    useEffect(() => {
+        // スクロールが存在する場合、メッセージ表示部分のスクロールを最下層に初期値として設定
+        if(messageArea.current !== null) {
+            messageArea.current.scrollTop = messageArea.current?.clientHeight;
+        }
+    }, []);
 
     return (
         <div id="talk">
             <Grid container justify="center">
-                <Grid item xs={11} sm={8} md={7} className="message_area">
+                <Grid item xs={11} sm={8} md={7} className="message_area" ref={messageArea}>
                     {
                         _.map(talk_list, value => (
                             <div className="message_box">
                                 {
                                     value.own_id === loginId ? 
                                         <Box component="div" key={value.id} m={1} borderRadius={16} className="right-box">
-                                            <Avatar
-                                                src={value.image_file}
-                                                className="avatar"
-                                            />
-                                            <CardContent>
+                                            <div className="avatar_area">
+                                                <Avatar
+                                                    src={value.image_file}
+                                                    className="avatar"
+                                                />
+                                                <Typography className="avatar_name">
+                                                    {value.user_name}
+                                                </Typography>
+                                            </div>
+                                            <div className="content_area">
                                                 <Typography key={value.id} className="content">{value.content}</Typography>
-                                            </CardContent>
+                                            </div>
                                         </Box>
                                     :
                                         <Box component="div" key={value.id} m={1} borderRadius={16} className="left-box">
-                                            <Avatar
-                                                src={value.image_file}
-                                                className="avatar"
-                                            />
-                                            <CardContent>
+                                            <div className="avatar_area">
+                                                <Avatar
+                                                    src={value.image_file}
+                                                    className="avatar"
+                                                />
+                                                <Typography className="avatar_name left">
+                                                    {value.user_name}
+                                                </Typography>
+                                            </div>
+                                            <div className="content_area">
                                                 <Typography key={value.id} className="content">{value.content}</Typography>
-                                            </CardContent>
+                                            </div>
                                         </Box>
                                 }
                             </div>
