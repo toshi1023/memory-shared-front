223c6fb4deb62ccba12e4e380f599cccc32af94a
toshi1023 <toshimura1992@gmail.com>
2021-09-04 12:53:03 +0000

    appSlice implements

--- src/views/pages/appSlice.ts
+++ src/views/pages/appSlice.ts
@@ -1,3 +1,50 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../../stores/store";
-import axios from "axios";
+
+/**
+ * Slice(store)の設定
+ */
+ const appSlice = createSlice({
+    name: 'app',
+    // stateの初期状態
+    initialState: {
+        // isLoading: ローディングフラグを管理
+        isLoading: false,
+        // infoメッセージの管理
+        infoMessages: '',
+        // errorメッセージの管理
+        errorMessages: '',
+    },
+    // Reducer (actionの処理を記述)
+    reducers: {
+        // Lodingの開始
+        fetchCredStart(state) {
+            state.isLoading = true;
+        },
+        // Lodingの終了
+        fetchCredEnd(state) {
+            state.isLoading = false;
+        },
+        // infoメッセージの取得
+        fetchGetInfoMessages(state, action) {
+            state.infoMessages = action.payload
+        },
+        // errorメッセージの取得
+        fetchGetErrorMessages(state, action) {
+            state.errorMessages = action.payload
+        }
+    }
+})
+
+export const {
+    fetchCredStart,
+    fetchCredEnd,
+    fetchGetInfoMessages,
+    fetchGetErrorMessages,
+} = appSlice.actions;
+
+export const selectLoading = (state: RootState) => state.app.isLoading;
+export const selectInfoMessage = (state: RootState) => state.app.infoMessages;
+export const selectErrorMessage = (state: RootState) => state.app.errorMessages;
+
+export default appSlice.reducer
